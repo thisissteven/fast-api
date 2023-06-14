00f22c32bce42d1469ee0fff7e06c6da10a422fa
Steven <steven04@ui.ac.id~>
2023-06-14 13:47:53 +0700

    finish api

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
 # README
-# Hello everyone, in here I (Kaenova | Bangkit Mentor ML-20) 
-# will give you some headstart on createing ML API. 
-# Please read every lines and comments carefully. 
-# 
-# I give you a headstart on text based input and image based input API. 
+# Hello everyone, in here I (Kaenova | Bangkit Mentor ML-20)
+# will give you some headstart on createing ML API.
+# Please read every lines and comments carefully.
+#
+# I give you a headstart on text based input and image based input API.
 # To run this server, don't forget to install all the libraries in the
-# requirements.txt simply by "pip install -r requirements.txt" 
+# requirements.txt simply by "pip install -r requirements.txt"
 # and then use "python main.py" to run it
-# 
+#
 # For ML:
 # Please prepare your model either in .h5 or saved model format.
 # Put your model in the same folder as this main.py file.
-# You will load your model down the line into this code. 
-# There are 2 option I give you, either your model image based input 
+# You will load your model down the line into this code.
+# There are 2 option I give you, either your model image based input
 # or text based input. You need to finish functions "def predict_text" or "def predict_image"
-# 
+#
 # For CC:
-# You can check the endpoint that ML being used, eiter it's /predict_text or 
+# You can check the endpoint that ML being used, eiter it's /predict_text or
 # /predict_image. For /predict_text you need a JSON {"text": "your text"},
-# and for /predict_image you need to send an multipart-form with a "uploaded_file" 
+# and for /predict_image you need to send an multipart-form with a "uploaded_file"
 # field. you can see this api documentation when running this server and go into /docs
 # I also prepared the Dockerfile so you can easily modify and create a container iamge
 # The default port is 8080, but you can inject PORT environement variable.
-# 
+#
 # If you want to have consultation with me
 # just chat me through Discord (kaenova#2859) and arrange the consultation time
 #
@@ -54,7 +54,8 @@ from utils import load_image_into_numpy_array
 # If you use saved model type uncomment line below
 
 # Set the experimental_io_device option
-load_options = tf.saved_model.LoadOptions(experimental_io_device='/job:localhost')
+load_options = tf.saved_model.LoadOptions(
+    experimental_io_device='/job:localhost')
 
 # Load the SavedModel with the specified load options
 model = tf.saved_model.load("./my_model_folder", options=load_options)
@@ -62,15 +63,26 @@ model = tf.saved_model.load("./my_model_folder", options=load_options)
 app = FastAPI()
 
 # This endpoint is for a test (or health check) to this server
+
+
 @app.get("/")
 def index():
     return "Hello world from ML endpoint!"
 
 # If your model need text input use this endpoint!
+
+
 class RequestText(BaseModel):
-    text:str
+    text: str
+
+
+labels = ['fresh apple', 'stale apple', 'fresh bitter gourd', 'stale bitter gourd',
+          'fresh banana', 'stale banana', 'fresh capsicum', 'stale capsicum', 'fresh orange',
+          'stale orange', 'fresh tomato', 'stale tomato', 'fresh meat', 'spoiled meat']
 
 # If your model need image input use this endpoint!
+
+
 @app.post("/predict_image")
 def predict_image(uploaded_file: UploadFile, response: Response):
     try:
@@ -78,7 +90,7 @@ def predict_image(uploaded_file: UploadFile, response: Response):
         if uploaded_file.content_type not in ["image/jpeg", "image/png"]:
             response.status_code = 400
             return "File is Not an Image"
-        
+
         # In here you will get a numpy array in "image" variable.
         # You can use this file, to load and do processing
         # later down the line
@@ -86,26 +98,18 @@ def predict_image(uploaded_file: UploadFile, response: Response):
         # Step 1: (Optional, but you should have one) Do your image preprocessing
         image = np.resize(image, (150, 150, 3)) / 255.0
 
-        print("Image shape:", image.shape)
-        
-        
         # Step 2: Prepare your data for your model
         input_data = tf.constant([image], dtype=tf.float32)
-        
+
         # Step 3: Predict the data
         result = model(input_data)
-        labels = ['fresh_apple','stale_apple', 'fresh_bitter_gourd', 'stale_bitter_gourd',
-          'fresh_banana', 'stale_banana', 'fresh_capsicum', 'stale_capsicum', 'fresh_orange',
-          'stale_orange', 'fresh_tomato', 'stale_tomato', 'fresh_meat', 'spoiled_meat']
-        
+        tensor = tf.constant(result)
 
-         # Step 4: Map the result to the labels
-        prediction_index = np.argmax(result)
-        predicted_label = labels[prediction_index]
-        
+        results_array = tensor.numpy()[0].tolist()
+
         return json.dumps({
-            'label': predicted_label,
-            # 'accuracy': prediction_index
+            'labels': labels,
+            'results': results_array
         })
     except Exception as e:
         traceback.print_exc()
@@ -117,4 +121,4 @@ def predict_image(uploaded_file: UploadFile, response: Response):
 # Your can check the API documentation easily using /docs after the server is running
 port = os.environ.get("PORT", 8080)
 print(f"Listening to http://0.0.0.0:{port}")
-uvicorn.run(app, host='0.0.0.0',port=port)
+uvicorn.run(app, host='0.0.0.0', port=port)
